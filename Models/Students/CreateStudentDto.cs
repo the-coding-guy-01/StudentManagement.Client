98ea1369064a3a1a38373bd686b84a7aaf5d70ba
the-coding-gut-01 <thecodingguy01@gmail.com>
2026-09-18 09:34:02 +0530

    Implemented partial student CRUD

---
+++ Models/Students/CreateStudentDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Client.Models.Students
+{
+    public class CreateStudentDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime? DateOfBirth { get; set; }
+
+        [MaxLength(20)]
+        public string Gender { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+
+        public DateTime EnrollmentDate { get; set; } = DateTime.UtcNow;
+
+        public bool isActive { get; set; } = true;
+    }
+}
---
+++ Models/Students/StudentDto.cs
@@ -0,0 +1,27 @@
+﻿namespace StudentManagement.Client.Models.Students
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+
+        public string AdmissionNumber { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Gender { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
